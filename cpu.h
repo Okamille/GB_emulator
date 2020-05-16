f07e0d7568edd8375bd331e91ac8e678955f39db
Antoine Gauthier <antoine.gauthier@ensta.fr>
2020-05-16 09:54:27 +0200

    Added register data structure

--- cpu.h
+++ cpu.h
@@ -2,7 +2,9 @@
 #define CPU_H
 
 #include "utils.h"
+#include "registers.h"
 
 int nextOpCode(BYTE opcode);
+
 
 #endif
--- registers.h
+++ registers.h
@@ -1,4 +1,24 @@
 #ifndef REGISTERS_H
 #define REGISTERS_H
 
+#include "utils.h"
+
+// Registers in CPU
+
+union Register
+{
+    WORD reg;
+    struct
+    {
+        BYTE lo;
+        BYTE hi;
+    };
+    
+};
+
+Register registerAF;
+Register registerBC;
+Register registerDE;
+Register registerHL;
+
 #endif
